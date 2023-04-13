63952fcfa00d12209961c3bc3fcebde22ddcd9e6
mm9136 <mm9136@student.uni-lj.si>
2023-04-13 18:43:32 +0200

    added stanza model to ner script

--- src/ner.py
+++ src/ner.py
@@ -1,6 +1,10 @@
 import spacy
+import stanza
 from spacy.lang.en.stop_words import STOP_WORDS
 nlp = spacy.load("en_core_web_sm", disable=["tok2vec", "tagger", "parser", "attribute_ruler", "lemmatizer"])
+nlp_stanza = stanza.Pipeline(lang='en', processors='tokenize,ner', use_gpu=False)
+# import spacy_stanza
+# nlp_stanza = spacy_stanza.load_pipeline("en")
 
 def get_entity_spacy(story):
     doc = nlp(story)
@@ -9,6 +13,11 @@ def get_entity_spacy(story):
         if entity.label_ == "PERSON":
             entities.append(entity.text)
     return entities
+
+def get_entity_stanza(story):
+    doc = nlp_stanza(story)  # run annotation over a sentence
+    stanzaEntities = [ent.text for sent in doc.sentences for ent in sent.ents if ent.type == 'PERSON']
+    return stanzaEntities
 
 def clean_entities(entities):
     clean_ent = []
@@ -38,6 +47,13 @@ if __name__ == "__main__":
     #path = "data/shortStories/"
     f = open("../data/shortStories/The Most Dangerous Game.txt", "r")
     text = f.read()
+
     entities = get_entity_spacy(text)
+    # print("Spacy:", entities)
     rez = clean_entities(entities)
-    print(rez)
+    print("Spacy:", rez)
+
+    entitiesStanza = get_entity_stanza(text)
+    # print("Stanza:", entitiesStanza)
+    rezStanza = clean_entities(entitiesStanza)
+    print("Stanza:", rezStanza)
