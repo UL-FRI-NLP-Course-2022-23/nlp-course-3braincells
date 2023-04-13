6a689b81401ef8020195b57d1b409cf0d0c97108
mm9136 <mm9136@student.uni-lj.si>
2023-04-13 12:49:19 +0200

    script for running models

---
+++ run_models.py
@@ -0,0 +1,11 @@
+import stanza
+import nltk
+#stanza
+stanza.download('en')
+
+#ntlk
+nltk.download('punkt')
+
+#spacy -> successfully dowloaded if no error after running this two lines
+# import spacy
+# nlp= spacy.load('en_core_web_sm')
