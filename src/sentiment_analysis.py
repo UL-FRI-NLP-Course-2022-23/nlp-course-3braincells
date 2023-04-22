2ef2a862deffa1e4bc55c485975e32b25f12bd5c
Ilina Kirovska <ik8739@student.uni-lj.si>
2023-04-22 23:56:36 +0200

    sentiment analysis afinn and stanza

--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -1,9 +1,10 @@
 import stanza
 from ner import get_entity_stanza, clean_entities
-
-
+from afinn import Afinn
+from nltk.tokenize import sent_tokenize
+afn = Afinn()
 # sentiment using stanza, argument is text and list of extracted characters
-def sentiment_stanza(text, characters_extracted):
+def sentiment_one_character_stanza(text, characters_extracted):
     # get sentiments using stanza for each sentence in text
     nlp = stanza.Pipeline(lang='en', processors='tokenize,sentiment')
     doc = nlp(text)
@@ -11,8 +12,8 @@ def sentiment_stanza(text, characters_extracted):
     stanzaEntities = {key: [] for key in characters_extracted}
     textSentiment = []
     for noSent, sent in enumerate(doc.sentences):
-        # get sentecens sentiment and because stanza returns 0,1,2 so subtract -1 to get real sentiment values
-        sentiment = sent.sentiment - 1
+        # get sentence sentiment and because stanza returns 0,1,2 subtract -1 to get real sentiment values
+        sentiment = sent.sentiment-1
         # convert object to list of dictionary
         sentence = sent.to_dict()
         textSentiment.append(sentiment)
@@ -24,8 +25,8 @@ def sentiment_stanza(text, characters_extracted):
                     stanzaEntities[entity].append(sentiment)
 
     # list of sentiment per character to most frequent values of list
-    # for key, value in stanzaEntities.items():
-    #     stanzaEntities[key] = listToValue(value)
+    for key, value in stanzaEntities.items():
+        stanzaEntities[key] = listToValue(value)
 
     # return text sentiment and character sentiment
     return listToValue(textSentiment), stanzaEntities
@@ -45,29 +46,93 @@ def listToValue(sentimentList):
             num = i
     return num
 
+def sentiment_multiple_characters_stanza(text, characters):
+    nlp = stanza.Pipeline(lang='en', processors='tokenize,sentiment')
+    doc = nlp(text)
+
+    relationships = {}
+    #makes a dictionary where the keys are the combinations of the characters in "john/jane" form
+    for i in range(len(characters)):
+        for j in range(i + 1, len(characters)):
+            relationships[f"{characters[i]}/{characters[j]}"] = []
+
+    for sent in doc.sentences:
+        sentence = sent.to_dict()
+        words = []
+        for word in sentence:
+            words.append(word['text'].lower())
+        for i in range(len(characters)):
+            for j in range(i+1, len(characters)):
+                    if characters[i] in words and characters[j] in words:
+                        relationships[f"{characters[i]}/{characters[j]}"].append(sent.sentiment - 1)
+    return relationships
+
+def evaluate_afinn(score):
+    if score > 0:
+        return 1
+    elif score == 0:
+        return 0
+    else:
+        return -1
+def eval_afinn(sentences, i, offset):
+    text = ""
+    if offset == 0:
+        score = afn.score(sentences[i])
+    else:
+        for j in range(i - offset, i + offset + 1):
+            if j >= 0 and j<len(sentences):
+                text += sentences[j]
+        score = afn.score(text)
+    if score > 0:
+        return 1
+    elif score == 0:
+        return 0
+    else:
+        return -1
+#function that evaluates the character sentiment based on the sentence in which the character is present
+#offset 0: evaluate only current sentence(contains character)
+#offset x: evaluate current sentence and sentences +/- x
+def sentiment_afinn(text, characters, offset):
+    sentences = sent_tokenize(text)
+    sentiments = {key: [] for key in characters}
+    for i in range(len(sentences)):
+        for character in characters:
+            if character in sentences[i].lower():
+                score = eval_afinn(sentences, i, offset)
+                sentiments[character].append(score)
+
+    # list of sentiment per character to most frequent values of list
+    for key, value in sentiments.items():
+         sentiments[key] = listToValue(value)
+    return sentiments
 
 if __name__ == "__main__":
     f = open("../data/our_data/cinderella.txt", "r", encoding='utf-8')
     text = f.read()
 
-    # NER: list of character extracted
-    entitiesStanza = get_entity_stanza(text)
-    rezStanza = clean_entities(entitiesStanza)
-    print(rezStanza)
-    ##### ['cinderella', 'king', 'prince', 'charlotte']
 
-    # sentiment using stanza
-    textSentiment ,sentiment = sentiment_stanza(text, rezStanza)
+    #ground truth character list
+    characters =  ["cinderella", "stepmother", "sisters", "godmother", "prince", "king", "queen"]
+    # # NER: list of character extracted
+    #entitiesStanza = get_entity_stanza(text)
+    # characters = clean_entities(entitiesStanza)
+    # print(characters)
+    # ##### ['cinderella', 'king', 'prince', 'charlotte']
+    #
+    #SENTIMENT USING STANZA
+    textSentiment ,sentiment = sentiment_one_character_stanza(text, characters)
+    print("Stanza")
     print(sentiment)
-    ##### {'cinderella': [0, 1, -1, 1, 0, 1, -1, 0, 0, 1, 0, -1, -1, 0, 0, -1, 1, 0, 0, 0, -1, 0, 0, 1, 1, 0, 1], 'king': [0, 0, 1, 0, 0, 0, -1, 0, -1], 'prince': [0, 0, 0, 0, 0], 'charlotte': [0, -1]}
-    ##### {'cinderella': 0, 'king': 0, 'prince': 0, 'charlotte': 0}
-    print(textSentiment)
+    #res: {'cinderella': 0, 'stepmother': 0, 'sisters': 0, 'godmother': 0, 'prince': 0, 'king': 0, 'queen': 1}
 
-    # NER: list of character extracted
-    char = ["cinderella", "stepmother", "sisters", "godmother", "prince", "king", "queen"]
-    # sentiment using stanza
-    textSentiment, sentiment = sentiment_stanza(text, char)
-    print(sentiment)
-    ##### {'cinderella': [0, 1, -1, 1, 0, 1, -1, 0, 0, 1, 0, -1, -1, 0, 0, -1, 1, 0, 0, 0, -1, 0, 0, 1, 1, 0, 1], 'stepmother': [0, 0], 'sisters': [-1, 1, -1, 0, 0, 1, 0, 0, -1, 0, 1, 0, 1], 'godmother': [0, 0, 0, 0, -1, 0, 1, 1, 1, 0, 0, 0, 0, 1], 'prince': [0, 0, 0, 0, 0], 'king': [0, 0, 1, 0, 0, 0, -1, 0, -1], 'queen': [1]}
-    ##### {'cinderella': 0, 'stepmother': 0, 'sisters': 0, 'godmother': 0, 'prince': 0, 'king': 0, 'queen': 1}
-    print(textSentiment)
+    relationships = sentiment_multiple_characters_stanza(text, characters)
+    print(relationships)
+    #res: {'cinderella/stepmother': [], 'cinderella/sisters': [1, -1, 0, 0, 0, 1, 1], 'cinderella/godmother': [0, 0, 1], 'cinderella/prince': [], 'cinderella/king': [], 'cinderella/queen': [], 'stepmother/sisters': [], 'stepmother/godmother': [], 'stepmother/prince': [], 'stepmother/king': [], 'stepmother/queen': [], 'sisters/godmother': [0], 'sisters/prince': [0], 'sisters/king': [], 'sisters/queen': [], 'godmother/prince': [], 'godmother/king': [0], 'godmother/queen': [], 'prince/king': [], 'prince/queen': [], 'king/queen': [1]}
+
+    #USING AFINN
+    print(sentiment_afinn(text,characters, 0))
+    #res: {'cinderella': 1, 'stepmother': -1, 'sisters': 1, 'godmother': 1, 'prince': 1, 'king': 1, 'queen': 1}
+    print(sentiment_afinn(text,characters, 1))
+    #res: {'cinderella': 1, 'stepmother': 1, 'sisters': 1, 'godmother': 1, 'prince': 1, 'king': 1, 'queen': 1}
+    print(sentiment_afinn(text, characters, 2))
+    #res: {'cinderella': 1, 'stepmother': 1, 'sisters': 1, 'godmother': 1, 'prince': 1, 'king': 1, 'queen': 1}
