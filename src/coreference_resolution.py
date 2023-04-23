5fd9ef8ff7ae483de9c75690dd435b0ba49aefb3
Ilina Kirovska <ik8739@student.uni-lj.si>
2023-04-23 20:58:41 +0200

    start of coref resolution

--- src/coreference_resolution.py
+++ src/coreference_resolution.py
@@ -1,3 +1,12 @@
-from corenlp import *
-corenlp = StanfordCoreNLP()  # wait a few minutes...
-corenlp.parse("Parse this sentence.")
+from pycorenlp import StanfordCoreNLP
+import json
+nlp = StanfordCoreNLP('http://localhost:9000')
+res = nlp.annotate("Obama is the the president of US. Florida is a nice place. It is good. He lives in Florida. Trump is the current president. He owns Trump tower.",
+                   properties={
+                       'annotators': 'coref',
+                       'outputFormat': 'json',
+                       'timeout': 75000,
+                   })
+# print(res)
+result = json.loads(res)
+print(result['corefs'])
